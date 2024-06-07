f40c67d90c82ef08c591fcaf56142fb0460b0ef5
maadesh <151980884+Maadesh11@users.noreply.github.com>
2024-06-07 18:05:35 +0530

    Add files via upload

---
+++ compar&assingmentoperators.js
@@ -0,0 +1,22 @@
+let a=100;
+let b=20;
+document.write(a==b," ");
+
+document.write(a!=b," ");
+document.write(a>b," ");
+document.write(a<b," ");
+document.write(a>=b," ");
+document.write(a<=b," ");
+document.write(a===b," ");
+document.write(a!==b," ");
+document.write("assigmentoperators");
+document.write(a+=b," ");
+document.write(a-=b," ");
+document.write(a*=b," ");
+document.write(a%=b," ");
+document.write(a/=b," ");
+
+
+
+
+
